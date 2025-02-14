420210671275db5b2085be211358355bd269eea0
JPaceGTC <jpace@globaltechinc.com>
2025-02-13 17:56:11 -0700

    Initial Update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='randl_client',
+    version='1',
+    packages=find_packages(),
+    url='https://github.com/JPaceGTC/randl_client',
+    author='jpace',
+)
