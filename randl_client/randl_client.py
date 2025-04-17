432fcbe6591d21b64eac0f94e33e49e0c4f6a48d
JPaceGTC <jpace@globaltechinc.com>
2025-04-17 17:07:34 -0600

    Update randl_client.py

--- randl_client/randl_client.py
+++ randl_client/randl_client.py
@@ -9,6 +9,7 @@ import sys
 import math
 import ast
 from io import StringIO
+from statistics import mean
 
 
 class Randl:    
@@ -27,14 +28,13 @@ class Randl:
         self.window_length = '1800'
         self.window_min_phases_needed= '5'
         self.window_exclude_associated_phases= 'False'
-        #self.window_exclude_duplicate_stations= 'True'
         self.window_step_size = 1
         
         #TODO finish setters for dml variables
         self.dml_models = ['pwave']
         self.dml_sampling = ['full']
         self.dml_num_samples = '10'
-        self.dml_arids = ['None'] # Update this to something cleaner/more robust
+        self.dml_arids = ['None'] 
         self.dml_pwave_modelpath = 'None'
         self.dml_exclude_duplicate_stations= 'True'
         self.dml_baz_modelpath = 'None'
@@ -206,9 +206,7 @@ class Randl:
         response = requests.post(url, headers=headers, data=json.dumps(req))
 
         # Check the response
-        if response.status_code == 200:
-            print("Success")
-        else:
+        if response.status_code != 200:
             print("Error:", response.status_code, response.text)
             return None         
         
@@ -238,9 +236,7 @@ class Randl:
 
         response = requests.post(url, headers=headers, data=json.dumps(req))
 
-        if response.status_code == 200:
-            print("Success")
-        else:
+        if response.status_code != 200:
             print("Error:", response.status_code, response.text)
             return None
         window = pd.read_json(StringIO(response.json()["result"]))
@@ -260,10 +256,7 @@ class Randl:
             "Content-Type": "application/json"
         }
         response = requests.post(url, headers=headers, data=json.dumps(req))
-        if response.status_code == 200:
-            print("Success")
-        else:
-            # If the request failed, print the error details
+        if response.status_code != 200:
             print("Error:", response.status_code, response.text)
             return None
             
@@ -286,17 +279,163 @@ class Randl:
             "Content-Type": "application/json"
         }
         response = requests.post(url, headers=headers, data=json.dumps(req))
-        if response.status_code == 200:
-            print("Success")
-        else:
+        if response.status_code != 200:
             # If the request failed, print the error details
             print("Error:", response.status_code, response.text)
             
         return response.json()["result"]
 
+
+
+    def taup_surrogate(self, inputs):
+        req = {"inputs": inputs}
+
+        url = self.url_base + "taup_surrogate"
+
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(req))
+        if response.status_code != 200:
+            print("Error:", response.status_code, response.text)
+            return None
+            
+        return response.json()["result"]
+
+
+    def baz_surrogate(self, inputs):
+        req = {"inputs": inputs}
+
+        url = self.url_base + "baz_surrogate"
+
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(req))
+        if response.status_code != 200:
+            print("Error:", response.status_code, response.text)
+            return None
+            
+        return response.json()["result"]
+
+
+    def baz_geo_surrogate(self, source_lat, source_lon, st_lat, st_lon):
+        req = {"source_lat": source_lat, "source_lon": source_lon, "st_lat": st_lat, "st_lon": st_lon}
+        print("326 req:", req)
+
+        url = self.url_base + "baz_geo_surrogate"
+
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(req))
+        if response.status_code != 200:
+            print("Error:", response.status_code, response.text)
+            return None
+            
+        return response.json()["result"]
+        #return pd.read_json(StringIO(response.json()["result"]))         
+
+
+
+    def associate_bulletin(self, bulletin, required_phases=5, exclude_associated_phases=False, travel_time=900, verbose=True):
+        origins = pd.DataFrame(columns=['Window_start', 'Window_end', 'DML_mean_lat', 'DML_mean_lon', 'Beamsearch_lat', 
+                                        'Beamsearch_lon', 'Beamsearch_time', 'associated_arids', 'Beamsearch_score'])
+
+        window_length = travel_time
+        self.set_window_length(window_length)
+        self.set_window_phases_required(required_phases)
+        self.set_window_exclude_associated_phases(exclude_associated_phases)
+
+
+        starttime  = parser.parse(bulletin.iloc[0]['TIME_ARRIV'])
+        bulletin_end = parser.parse(bulletin.iloc[len(bulletin)-1]['TIME_ARRIV'])
+
+        associated_arids = []
+
+        while starttime < bulletin_end:
+            if verbose:
+                print("Starting window at", starttime.strftime('%Y-%m-%dT%H:%M:%S'))
+            self.set_window_start(starttime.strftime('%Y-%m-%dT%H:%M:%S'))
+            window = self.window_catalog(bulletin)
+
+            try:
+                if len(window) > 0:
+                    if verbose:
+                        print("Associated arrivals in window:  ", len(window))
+
+                window = window[~window['ARID'].isin(associated_arids)]
+                if verbose:
+                    print("Unassociated arrivals in window:", len(window))
+                if len(window) > 4:
+
+                    window_end = parser.parse(window.TIME_ARRIV.iloc[len(window)-1])
+                    window_start = parser.parse(window.TIME_ARRIV.iloc[0])
+
+                    dml_predictions = self.dml_prediction(window)
+
+                    beam_result = self.beamsearch(window, dml_predictions)
+
+                    dml_lat_mean = mean(dml_predictions.LAT_ORIG)
+                    dml_lon_mean = mean(dml_predictions.LON_ORIG)
+
+                    beamsearch_lon = beam_result['unscaled_centroid'][0]
+                    beamsearch_lat = beam_result['unscaled_centroid'][1]
+                    beamsearch_elev = beam_result['unscaled_centroid'][2]
+                    beamsearch_x = beam_result['scaled_centroid'][0]
+                    beamsearch_y = beam_result['scaled_centroid'][1]
+                    beamsearch_z = beam_result['scaled_centroid'][2]
+                    beamsearch_time = parser.parse(beam_result['time'])
+
+                    result=pd.DataFrame()
+                    result['Window_start'] = [window_start]
+                    result['Window_end'] = [window_end]
+                    result['DML_mean_lat'] = [dml_lat_mean]
+                    result['DML_mean_lon'] = [dml_lon_mean]
+                    result['Beamsearch_lat'] = [beamsearch_lat]
+                    result['Beamsearch_lon'] = [beamsearch_lon]
+                    result['Beamsearch_elev'] = [beamsearch_elev]
+                    result['Beamsearch_x'] = [beamsearch_x]
+                    result['Beamsearch_y'] = [beamsearch_y]
+                    result['Beamsearch_z'] = [beamsearch_z]
+                    result['Beamsearch_time'] = [beamsearch_time]
+                    result['Beamsearch_score'] = [beam_result['score']]
+                    result['associated_arids'] = [beam_result['used_arids']]
+                    origins = pd.concat([origins, result], ignore_index=True)
+
+
+                    if len(beam_result['used_arids']) < 5:
+                        if verbose:
+                            print("No quality beams found")
+                        starttime += timedelta(seconds=window_length)
+                        continue
+
+                    associated_arids.extend(beam_result['used_arids'])
+
+                    window = window[~window['ARID'].isin(associated_arids)]
+
+                    if len(window) > 5:
+                        starttime = parser.parse(window.iloc[0]['TIME_ARRIV'])
+                    else:
+                        starttime += timedelta(seconds=window_length)
+
+                else:
+                    starttime += timedelta(seconds=window_length)
+                    if verbose:
+                        print("No arrivals. Moving window to start at", starttime.strftime('%Y-%m-%dT%H:%M:%S'))
+            except Exception as e:
+                starttime += timedelta(seconds=window_length)
+                print(e)
+                continue
+        print(len(origins), "origins found in bulletin.")        
+        return origins
+
             
     def __repr__ (self):
-        return "URL:" + self.url_base + "\n\n-Bulletin-\nStart:\t\t" + self.bulletin_start + "\nEnd:\t\t" + self.bulletin_end \
+        return "URL2:" + self.url_base + "\n\n-Bulletin-\nStart:\t\t" + self.bulletin_start + "\nEnd:\t\t" + self.bulletin_end \
     + "\nStations:\t" + self.bulletin_n_stations + "\nEvents:\t\t" + self.bulletin_n_events \
     + "\nDrop fraction:\t" + self.bulletin_drop_fraction + "\nSeed:\t" + self.bulletin_seed + "\n\n-Window-\nStart:\t\t\t" + self.window_start \
     + "\nLength:\t\t\t" + self.window_length + "\nMin_phases:\t\t" + self.window_min_phases_needed \
